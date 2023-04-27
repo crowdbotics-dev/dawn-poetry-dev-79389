5cffdc50621b32c9c2a63d63f140626b5433a5af
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 13:04:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dawn_poetry_dev_79389/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dawn_poetry_dev_79389;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dawn_poetry_dev_79389";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-poetry-dev-79389",
+  url: "https://dawn-poetry-dev-79389.botics.co",
+  api: "https://dawn-poetry-dev-79389.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
